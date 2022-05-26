578ab1596ac7e282b989a4e4d564eee95cdd4cf5
Barochkin_MS <Barochkin_MS@teleformis.ru>
2022-05-26 23:35:09 +0300

    Custom Draggable.

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,47 +1,20 @@
-import React, {PropsWithChildren, useCallback, useEffect, useRef} from "react";
+import React, {PropsWithChildren} from "react";
+import {useDragContext} from "../hooks/useDragContext";
 
 const Draggable: React.FC<PropsWithChildren<{ onDrag: (e: MouseEvent) => void, dragId?: string }>> = (
   {
     children,
     onDrag
   }): React.ReactElement => {
-  const dragRef = useRef<boolean>(false);
-  const timeoutRef = useRef<ReturnType<typeof setTimeout> | undefined>();
-  const dragStart = useCallback(() => {
-    timeoutRef.current = setTimeout(() => {
-      dragRef.current = true;
-    }, 200);
-  }, []);
-
-  useEffect(() => {
-    const dragend = () => {
-      if (timeoutRef.current) {
-        clearInterval(timeoutRef.current);
-        timeoutRef.current = undefined;
-      }
-
-      setTimeout(() => dragRef.current = false, 0);
-    };
-    const mousemove = (event: MouseEvent) => dragRef.current && onDrag(event);
-    const click = (event: MouseEvent) => dragRef.current && event.stopPropagation();
-
-    document.addEventListener("mousemove", mousemove, true);
-    document.addEventListener("mouseup", dragend, true);
-    document.addEventListener("click", click, true);
-
-    return () => {
-      document.removeEventListener("mousemove", mousemove, true);
-      document.removeEventListener("mouseup", dragend, true);
-      document.removeEventListener("click", click, true);
-    };
-  }, [onDrag]);
+  const [onMouseDown] = useDragContext();
 
   return (
     <>
       {React.Children.map(children, child => React.isValidElement(child)
         ? React.cloneElement(child, {
           "aria-details": "draggable",
-          onMouseDown: dragStart,
+          onMouseDown: (event: MouseEvent) => onMouseDown(event, onDrag, child),
+          onClick: console.log,
         })
         : null)}
     </>
--- src/hooks/useDragContext.ts
+++ src/hooks/useDragContext.ts
@@ -1,13 +1,65 @@
-import React, {useEffect} from "react";
+import {ReactElement} from "react";
+
+type MouseListener = (event: MouseEvent) => void;
+type DragEventInitiator = (event: MouseEvent, onDrag: MouseListener, element: ReactElement) => void;
 
 class DragContext {
-  refs: Set<React.RefObject<HTMLDivElement>> = new Set<React.RefObject<HTMLDivElement>>();
+  private element: ReactElement | undefined;
+  private initialPosition: Record<"x" | "y", number> | undefined;
+  private onDrag: MouseListener | undefined;
+
+  initiator: MouseListener = (event) => {
+    const position = {x: event.clientX || event.pageX, y: event.clientY || event.pageY};
+    if (!this.initialPosition) {
+      this.clear();
+      return;
+    }
+
+    const xChanged = Math.abs(position.x - this.initialPosition.x) > 20;
+    const yChanged = Math.abs(position.y - this.initialPosition.y) > 20;
+
+    if (xChanged || yChanged) {
+      // Prevent click then Drag!
+      document.addEventListener("click", this.stopPropagation, true);
+      document.removeEventListener("mousemove", this.initiator, true);
+      document.addEventListener("mousemove", this.onMouseMove, true);
+    }
+  }
+
+  onMouseDown: DragEventInitiator = (event, onDrag, element) => {
+    this.initialPosition = {x: event.clientX || event.pageX, y: event.clientY || event.pageY};
+    this.onDrag = onDrag;
+    this.element = element;
+    document.addEventListener("mousemove", this.initiator, true);
+    document.addEventListener("mouseup", this.onMouseUp, true);
+  }
+
+  onMouseUp: MouseListener = () => {
+    this.clear();
+  }
+
+  onMouseMove: MouseListener = (event) => {
+    if (this.element && this.onDrag) {
+      this.onDrag(event);
+    }
+  }
+
+  stopPropagation: MouseListener = (event) => event.stopPropagation();
+
+  clear: () => void = () => {
+    this.onDrag = undefined;
+    this.element = undefined;
+    this.initialPosition = undefined;
+    document.removeEventListener("mousemove", this.initiator, true);
+    document.removeEventListener("mousemove", this.onMouseMove, true);
+    document.removeEventListener("mouseup", this.onMouseUp, true);
+    // Allow click!
+    setTimeout(() => document.removeEventListener("click", this.stopPropagation, true));
+  }
 }
 
 const context = new DragContext();
 
-export const useDragContext = (...refs: Array<React.RefObject<HTMLDivElement>>) => {
-  useEffect(() => {
-    refs.forEach(context.refs.add);
-  }, [refs]);
+export const useDragContext = () => {
+  return [context.onMouseDown];
 }
