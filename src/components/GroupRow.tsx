f2e0a38d312d22d631093b6460d9bdc3caeb3bdf
Barochkin_MS <Barochkin_MS@teleformis.ru>
2022-05-28 09:48:11 +0300

    Group chevron.

--- src/components/GroupRow.tsx
+++ src/components/GroupRow.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Row} from "../table/Table";
 import {Primitive} from "./TableRow";
+import Chevron from "./Chevron";
 
 type GroupRowProps = {
   row: Row;
@@ -11,14 +12,14 @@ type GroupRowProps = {
   expandRow: (id: string | number) => void;
 }
 
-const GroupRow: React.FC<GroupRowProps> = ({row, offset, index, rowHeight, expandRow}): React.ReactElement => {
+const GroupRow: React.FC<GroupRowProps> = ({row, offset, index, rowHeight, expandRow, expanded}): React.ReactElement => {
   return (
     <div
       key={row.id} className={"rs-group-row"}
       style={{transform: `translateY(${rowHeight * (offset + index)}px)`, height: `${rowHeight}px`}}
       onClick={() => expandRow(row.name as string)}>
-      <div>{row.name as Primitive}</div>
-      <div>({row.count as Primitive})</div>
+      <Chevron direction={expanded[row.name as string] ? "bottom" : "right"} />
+      <div>{row.name as Primitive} ({row.count as Primitive})</div>
     </div>
   );
 };
---
+++ src/utils/rowSelectCacheUtils.ts
@@ -0,0 +1,3 @@
+const rowSelectCacheUtils = () => {
+  // TODO Array to handle length. Cache an object of all selected rows to achieve fast selection of all.
+};
